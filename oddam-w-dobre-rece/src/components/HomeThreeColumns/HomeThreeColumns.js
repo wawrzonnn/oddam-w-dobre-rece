e1b72bd846bd123a2a0434b89a8b541fb688d4d0
wawrzonnn <norbert.wawrzonek@gmail.com>
2022-12-06 21:55:27 +0100

    add section ThreeColumns

--- oddam-w-dobre-rece/src/components/HomeThreeColumns/HomeThreeColumns.js
+++ oddam-w-dobre-rece/src/components/HomeThreeColumns/HomeThreeColumns.js
@@ -4,19 +4,28 @@ const HomeThreeColumns = () => {
 	return (
 		<div class='threecolumns__box'>
 			<div className='threecolumns__box--small'>
-				<p>10</p>
-				<p>ODDANYCH WORKÓW</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+				<p className='threecolumns__title--1'>10</p>
+				<p className='threecolumns__title--2'>ODDANYCH WORKÓW</p>
+				<p className='threecolumns__title--3'>
+					Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat
+					volutpat.
+				</p>
 			</div>
 			<div className='threecolumns__box--small'>
-				<p>5</p>
-				<p>WSPARTYCH ORGANIZACJI</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+				<p className='threecolumns__title--1'>5</p>
+				<p className='threecolumns__title--2'>WSPARTYCH ORGANIZACJI</p>
+				<p className='threecolumns__title--3'>
+					Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat
+					volutpat.
+				</p>
 			</div>
 			<div className='threecolumns__box--small'>
-				<p>7</p>
-				<p>ZORGANIZOWANYCH ZBIÓREK</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+				<p className='threecolumns__title--1'>7</p>
+				<p className='threecolumns__title--2'>ZORGANIZOWANYCH ZBIÓREK</p>
+				<p className='threecolumns__title--3'>
+					Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat
+					volutpat.
+				</p>
 			</div>
 		</div>
 	)
