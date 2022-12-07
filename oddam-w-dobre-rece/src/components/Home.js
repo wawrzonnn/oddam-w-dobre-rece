39a64461728d070e4ad4951a5d795a1ac4b6b578
wawrzonnn <norbert.wawrzonek@gmail.com>
2022-12-07 21:13:24 +0100

    add section about us

--- oddam-w-dobre-rece/src/components/Home.js
+++ oddam-w-dobre-rece/src/components/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import HomeHeader from './HomeHeader/HomeHeader';
 import HomeThreeColumns from './HomeThreeColumns/HomeThreeColumns';
 import HomeSteps from './HomeSteps/HomeSteps';
+import HomeAbout from './HomeAbout/HomeAbout';
+
+
 const Home = () => {
 	return (
 		<>
 		<HomeHeader />
 		<HomeThreeColumns />
 		<HomeSteps />
+		<HomeAbout />
 		</>
 	)
 }
---
+++ oddam-w-dobre-rece/src/components/HomeAbout/HomeAbout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import people from '../../assets/People.jpg'
+import signature from '../../assets/Signature.svg'
+import decoration from '../../assets/Decoration.svg'
+
+const HomeAbout = () => {
+	return (
+		<>
+			<div className='homeabout__box'>
+				<div className='homeabout__box--title'>
+					<h2 className='global__title'>O nas</h2>
+					<img src={decoration} alt='dekoracja' />
+					<p className='homeabout__text'>
+						Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed
+						pea prairie turnip leek lentil turnip greens parsnip.
+					</p>
+					<img className='homeabout__signature' src={signature} alt='podpis' />
+				</div>
+				<div className='homeabout__box--img'>
+					<img className='homeabout__img--item' src={people} alt='ludzie' />
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default HomeAbout
